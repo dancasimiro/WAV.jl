ee0f78a1770f0e818bfbbbf257fdd39406ccb5e6
Daniel C. Casimiro <dan.casimiro@gmail.com>
2013-03-11 22:23:43 -0400

    add a function to decompression A-law encoded files.

--- src/WAV.jl
+++ src/WAV.jl
@@ -303,6 +303,59 @@ function read_mulaw_samples(io::IO, chunk_size::Unsigned, fmt::WAVFormat)
     return samples
 end
 
+function read_alaw_samples(io::IO, chunk_size::Unsigned, fmt::WAVFormat)
+    # Quantized A-law algorithm -- Use a look up table to convert
+    const ALawDecompressTable =
+    [
+    -5504, -5248, -6016, -5760, -4480, -4224, -4992, -4736, 
+    -7552, -7296, -8064, -7808, -6528, -6272, -7040, -6784, 
+    -2752, -2624, -3008, -2880, -2240, -2112, -2496, -2368, 
+    -3776, -3648, -4032, -3904, -3264, -3136, -3520, -3392, 
+    -22016,-20992,-24064,-23040,-17920,-16896,-19968,-18944, 
+    -30208,-29184,-32256,-31232,-26112,-25088,-28160,-27136, 
+    -11008,-10496,-12032,-11520,-8960, -8448, -9984, -9472, 
+    -15104,-14592,-16128,-15616,-13056,-12544,-14080,-13568, 
+    -344,  -328,  -376,  -360,  -280,  -264,  -312,  -296, 
+    -472,  -456,  -504,  -488,  -408,  -392,  -440,  -424, 
+    -88,   -72,   -120,  -104,  -24,   -8,    -56,   -40, 
+    -216,  -200,  -248,  -232,  -152,  -136,  -184,  -168, 
+    -1376, -1312, -1504, -1440, -1120, -1056, -1248, -1184, 
+    -1888, -1824, -2016, -1952, -1632, -1568, -1760, -1696, 
+    -688,  -656,  -752,  -720,  -560,  -528,  -624,  -592, 
+    -944,  -912,  -1008, -976,  -816,  -784,  -880,  -848, 
+    5504,  5248,  6016,  5760,  4480,  4224,  4992,  4736, 
+    7552,  7296,  8064,  7808,  6528,  6272,  7040,  6784, 
+    2752,  2624,  3008,  2880,  2240,  2112,  2496,  2368, 
+    3776,  3648,  4032,  3904,  3264,  3136,  3520,  3392, 
+    22016, 20992, 24064, 23040, 17920, 16896, 19968, 18944, 
+    30208, 29184, 32256, 31232, 26112, 25088, 28160, 27136, 
+    11008, 10496, 12032, 11520, 8960,  8448,  9984,  9472, 
+    15104, 14592, 16128, 15616, 13056, 12544, 14080, 13568, 
+    344,   328,   376,   360,   280,   264,   312,   296, 
+    472,   456,   504,   488,   408,   392,   440,   424, 
+    88,    72,   120,   104,    24,     8,    56,    40, 
+    216,   200,   248,   232,   152,   136,   184,   168, 
+    1376,  1312,  1504,  1440,  1120,  1056,  1248,  1184, 
+    1888,  1824,  2016,  1952,  1632,  1568,  1760,  1696, 
+    688,   656,   752,   720,   560,   528,   624,   592, 
+    944,   912,  1008,   976,   816,   784,   880,   848 
+     ]
+    @assert length(ALawDecompressTable) == 256
+    nsamples = uint(chunk_size / fmt.block_align)
+    nblocks = uint(nsamples * fmt.nchannels)
+    blocks = read(io, Uint8, nblocks)
+    samples = zeros(Int16, nsamples, fmt.nchannels)
+    for i = 1:nsamples
+        for j = 1:fmt.nchannels
+            index = int(blocks[(i - 1) * fmt.nchannels + j] + 1)
+            # add one to value from blocks because A-law stores values from 0 to 255.
+            # Julia indexing is 1-based; I need a value from 1 to 256
+            samples[i, j] = ALawDecompressTable[clamp(blocks[(i - 1) * fmt.nchannels + j] + 1, 1, 256)]
+        end
+    end
+    return samples
+end
+
 #function write_mulaw_samples(io::IO, chunk_size::Unsigned, fmt::WAVFormat)
 #    const cBias = 0x84
 #    const cClip = 32635
@@ -373,6 +426,8 @@ function read_data(io::IO, chunk_size::Uint32, fmt::WAVFormat, opts::Options)
         samples = read_ieee_float_samples(io, chunk_size, fmt)
     elseif fmt.compression_code == WAVE_FORMAT_MULAW
         samples = read_mulaw_samples(io, chunk_size, fmt)
+    elseif fmt.compression_code == WAVE_FORMAT_ALAW
+        samples = read_alaw_samples(io, chunk_size, fmt)
     else
         error("$(fmt.compression_code) is an unsupported compression code!")
     end
