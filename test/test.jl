9143689da57b5410cd7d048e804f85a37076fa92
Daniel C. Casimiro <dan.casimiro@gmail.com>
2013-10-19 13:17:07 -0400

    Address min/max deprecation warnings.
    Replace with minimum and maximum.

--- test/test.jl
+++ test/test.jl
@@ -5,7 +5,7 @@ reload("WAV.jl")
 # These float array comparison functions are from dists.jl
 function absdiff{T<:Real}(current::AbstractArray{T}, target::AbstractArray{T})
     @assert all(size(current) == size(target))
-    max(abs(current - target))
+    maximum(abs(current - target))
 end
 
 function reldiff{T<:Real}(current::T, target::T)
@@ -14,7 +14,7 @@ end
 
 function reldiff{T<:Real}(current::AbstractArray{T}, target::AbstractArray{T})
     @assert all(size(current) == size(target))
-    max([reldiff(current[i], target[i]) for i in 1:numel(target)])
+    maximum([reldiff(current[i], target[i]) for i in 1:numel(target)])
 end
 
 ## Test wavread and wavwrite
@@ -26,8 +26,8 @@ for fs = (8000,11025,22050,44100,48000,96000,192000), nbits = (1,7,8,9,12,16,20,
 
     in_data = rand(nsamples, nchans)
     if nsamples > 0
-        @assert max(in_data) <= 1.0
-        @assert min(in_data) >= -1.0
+        @assert maximum(in_data) <= 1.0
+        @assert minimum(in_data) >= -1.0
     end
     io = IOBuffer()
     WAV.wavwrite(in_data, io, Fs=fs, nbits=nbits, compression=WAV.WAVE_FORMAT_PCM)
@@ -61,7 +61,7 @@ for fs = (8000,11025,22050,44100,48000,96000,192000), nbits = (1,7,8,9,12,16,20,
     if nsamples > 0
         seek(io, 0)
         # Don't convert to Int, test if passing a float (nsamples/2) behaves as expected
-        subsamples = min(10, nsamples / 2)
+        subsamples = min(10, int(nsamples / 2))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=subsamples)
         @assert length(out_data) == subsamples * nchans
         @assert size(out_data, 1) == subsamples
@@ -73,7 +73,7 @@ for fs = (8000,11025,22050,44100,48000,96000,192000), nbits = (1,7,8,9,12,16,20,
         @assert absdiff(out_data, in_data[1:int(subsamples), :]) < tol
 
         seek(io, 0)
-        sr = convert(Int, min(5, nsamples / 2)):convert(Int, min(23, nsamples - 1))
+        sr = convert(Int, min(5, int(nsamples / 2))):convert(Int, min(23, nsamples - 1))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=sr)
         @assert length(out_data) == length(sr) * nchans
         @assert size(out_data, 1) == length(sr)
