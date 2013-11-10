322eac4005a437cffc196bd4225dad8c9dcb3491
Daniel C. Casimiro <dan.casimiro@gmail.com>
2013-11-10 05:00:46 -0500

    flush the IO stream in wavwrite when a filename is given.
    Otherwise, the file is not written to disk until the garbage collector runs.

--- src/WAV.jl
+++ src/WAV.jl
@@ -431,7 +431,8 @@ end
 function wavwrite(samples::Array, filename::String; Fs=8000, nbits=16, compression=WAVE_FORMAT_PCM)
     io = open(filename, "w")
     finalizer(io, close)
-    return wavwrite(samples, io, Fs=Fs, nbits=nbits, compression=compression)
+    wavwrite(samples, io, Fs=Fs, nbits=nbits, compression=compression)
+    flush(io)
 end
 
 wavwrite(y::Array, f::Real, filename::String) = wavwrite(y, filename, Fs=f)
