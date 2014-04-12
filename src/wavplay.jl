7578ce252c95fd2bb72a45d03e04615c78695b68
Jason Knight <Jason@JasonKnight.us>
2014-04-11 20:54:58 -0500

    Add wavplay function using Linux's PulseAudio
    Under linux, it is possible to play wav files without any prerequisites. In
    Ubuntu I had to install libpulse-dev, but Fedora had the libpulse-simple
    library already installed.

---
+++ src/wavplay.jl
@@ -0,0 +1,29 @@
+
+immutable PulseSample
+    format::Int32
+    rate::Uint32
+    channels::Uint8
+end
+
+wavplay(fname) = wavplay(wavread(fname)...)
+
+function wavplay(data, Fs, args...)
+    ss = PulseSample(5,Fs,size(data,2))
+    data = convert(Array{Float32}, data)
+
+    err = Cint[0]
+    s = ccall((:pa_simple_new, "libpulse-simple"), 
+        Ptr{Void}, (Ptr{Void}, Ptr{Uint8}, Cint, Ptr{Void}, Ptr{Uint8}, 
+        Ptr{PulseSample}, Ptr{Void}, Ptr{Void}, Ptr{Cint}), 
+        0,"hey",1,0,"playback",&ss,0,0,err)
+    assert(s != C_NULL)
+
+
+    assert(0 == ccall((:pa_simple_write, "libpulse-simple"), Int32, 
+        (Ptr{Void}, Ptr{Void}, Cssize_t, Ptr{Cint}),
+        s, data, sizeof(data), err))
+
+    assert(0 == ccall((:pa_simple_drain, "libpulse-simple"), Int32, 
+        (Ptr{Void}, Ptr{Cint}), s, err))
+    nothing
+end
