220b54fa697ec44e430aa2b565ec07de18eaa268
Joao Felipe Santos <joao.eel@gmail.com>
2014-11-06 11:31:44 -0500

    Added WAVArray and writemime methods

--- src/WAV.jl
+++ src/WAV.jl
@@ -1,9 +1,11 @@
 # -*- mode: julia; -*-
 module WAV
-export wavread, wavwrite, wavappend, wavplay, WAVE_FORMAT_PCM, WAVE_FORMAT_IEEE_FLOAT, WAVE_FORMAT_ALAW, WAVE_FORMAT_MULAW
+export wavread, wavwrite, wavappend, wavplay, WAVArray, WAVE_FORMAT_PCM, WAVE_FORMAT_IEEE_FLOAT, WAVE_FORMAT_ALAW, WAVE_FORMAT_MULAW
 import Base.unbox, Base.box
 
 @linux? include("wavplay.jl") : (wavplay(args...) = warn("wavplay is not currently implemented on $OS_NAME"))
+
+include("AudioDisplay.jl")
 
 # The WAV specification states that numbers are written to disk in little endian form.
 write_le(stream::IO, value::Uint8) = write(stream, value)
