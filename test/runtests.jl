02188276e789d520b64250dab24c0b5c62cbf3f7
Daniel C. Casimiro <daniel.casimiro@sonos.com>
2015-03-24 21:50:28 -0400

    Fix v0.4 deprecations in test runner
    Mostly, this required explicit truncations instead of relying on implicit behavior of
    convert.

--- test/runtests.jl
+++ test/runtests.jl
@@ -144,7 +144,7 @@ for fs = (8000,11025,22050,44100,48000,96000,192000), nbits = (1,7,8,9,12,16,20,
     if nsamples > 0
         seek(io, 0)
         # Don't convert to Int, test if passing a float (nsamples/2) behaves as expected
-        subsamples = min(10, int(nsamples / 2))
+        subsamples = min(10, trunc(Int, nsamples / 2))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=subsamples)
         @assert length(out_data) == subsamples * nchans
         @assert size(out_data, 1) == subsamples
@@ -153,10 +153,10 @@ for fs = (8000,11025,22050,44100,48000,96000,192000), nbits = (1,7,8,9,12,16,20,
         @assert out_fs == fs
         @assert out_nbits == nbits
         @assert out_extra == None
-        @assert absdiff(out_data, in_data[1:int(subsamples), :]) < tol
+        @assert absdiff(out_data, in_data[1:subsamples, :]) < tol
 
         seek(io, 0)
-        sr = convert(Int, min(5, int(nsamples / 2))):convert(Int, min(23, nsamples - 1))
+        sr = convert(Int, min(5, trunc(Int, nsamples / 2))):convert(Int, min(23, nsamples - 1))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=sr)
         @assert length(out_data) == length(sr) * nchans
         @assert size(out_data, 1) == length(sr)
@@ -183,7 +183,7 @@ x, fs, nbits, extra = WAV.wavread(io)
 
 ## Test native encoding of 8 bits
 for nchans = (1,2,4)
-    in_data_8 = uint8(reshape(int(typemin(Uint8):typemax(Uint8)), (int(256 / nchans), nchans)))
+    in_data_8 = reshape(typemin(Uint8):typemax(Uint8), (trunc(Int, 256 / nchans), nchans))
     io = IOBuffer()
     WAV.wavwrite(in_data_8, io)
 
@@ -197,7 +197,7 @@ end
 
 ## Test native encoding of 16 bits
 for nchans = (1,2,4)
-    in_data_16 = int16(reshape(int(typemin(Int16):typemax(Int16)), (int(65536 / nchans), nchans)))
+    in_data_16 = reshape(typemin(Int16):typemax(Int16), (trunc(Int, 65536 / nchans), nchans))
     io = IOBuffer()
     WAV.wavwrite(in_data_16, io)
 
@@ -211,7 +211,7 @@ end
 
 ## Test native encoding of 24 bits
 for nchans = (1,2,4)
-    in_data_24 = convert(Array{Int32}, reshape(-63:64, int(128 / nchans), nchans))
+    in_data_24 = convert(Array{Int32}, reshape(-63:64, trunc(Int, 128 / nchans), nchans))
     io = IOBuffer()
     WAV.wavwrite(in_data_24, io)
 
@@ -225,7 +225,7 @@ end
 
 ## Test encoding 32 bit values
 for nchans = (1,2,4)
-    in_data_single = convert(Array{Float32}, reshape(linspace(-1.0, 1.0, 128), int(128 / nchans), nchans))
+    in_data_single = convert(Array{Float32}, reshape(linspace(-1.0, 1.0, 128), trunc(Int, 128 / nchans), nchans))
     io = IOBuffer()
     WAV.wavwrite(in_data_single, io)
 
@@ -239,7 +239,7 @@ end
 
 ## Test encoding 32 bit values outside the [-1, 1] range
 for nchans = (1,2,4)
-    nsamps = int(128 / nchans)
+    nsamps = trunc(Int, 128 / nchans)
     in_data_single = convert(Array{Float32}, reshape(-63:64, nsamps, nchans))
     io = IOBuffer()
     WAV.wavwrite(in_data_single, io)
@@ -254,7 +254,7 @@ end
 
 ## Test encoding 64 bit values
 for nchans = (1,2,4)
-    in_data_single = convert(Array{Float64}, reshape(linspace(-1.0, 1.0, 128), int(128 / nchans), nchans))
+    in_data_single = convert(Array{Float64}, reshape(linspace(-1.0, 1.0, 128), trunc(Int, 128 / nchans), nchans))
     io = IOBuffer()
     WAV.wavwrite(in_data_single, io)
 
@@ -268,7 +268,7 @@ end
 
 ## Test encoding 64 bit values outside the [-1, 1] range
 for nchans = (1,2,4)
-    nsamps = int(128 / nchans)
+    nsamps = trunc(Int, 128 / nchans)
     in_data_single = convert(Array{Float64}, reshape(-63:64, nsamps, nchans))
     io = IOBuffer()
     WAV.wavwrite(in_data_single, io)
@@ -322,7 +322,7 @@ for nbits = (8, 16), nsamples = convert(Array{Int}, [0; logspace(1, 4, 4)]), nch
     if nsamples > 0
         seek(io, 0)
         # Don't convert to Int, test if passing a float (nsamples/2) behaves as expected
-        subsamples = min(10, int(nsamples / 2))
+        subsamples = min(10, trunc(Int, nsamples / 2))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=subsamples)
         @assert length(out_data) == subsamples * nchans
         @assert size(out_data, 1) == subsamples
@@ -334,7 +334,7 @@ for nbits = (8, 16), nsamples = convert(Array{Int}, [0; logspace(1, 4, 4)]), nch
         @assert absdiff(out_data, in_data[1:int(subsamples), :]) < tol
 
         seek(io, 0)
-        sr = convert(Int, min(5, int(nsamples / 2))):convert(Int, min(23, nsamples - 1))
+        sr = convert(Int, min(5, trunc(Int, nsamples / 2))):convert(Int, min(23, nsamples - 1))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=sr)
         @assert length(out_data) == length(sr) * nchans
         @assert size(out_data, 1) == length(sr)
@@ -388,7 +388,7 @@ for nbits = (32, 64), nsamples = convert(Array{Int}, [0; logspace(1, 4, 4)]), nc
     if nsamples > 0
         seek(io, 0)
         # Don't convert to Int, test if passing a float (nsamples/2) behaves as expected
-        subsamples = min(10, int(nsamples / 2))
+        subsamples = min(10, trunc(Int, nsamples / 2))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=subsamples)
         @assert length(out_data) == subsamples * nchans
         @assert size(out_data, 1) == subsamples
@@ -400,7 +400,7 @@ for nbits = (32, 64), nsamples = convert(Array{Int}, [0; logspace(1, 4, 4)]), nc
         @assert absdiff(out_data, in_data[1:int(subsamples), :]) < tol
 
         seek(io, 0)
-        sr = convert(Int, min(5, int(nsamples / 2))):convert(Int, min(23, nsamples - 1))
+        sr = convert(Int, min(5, trunc(Int, nsamples / 2))):convert(Int, min(23, nsamples - 1))
         out_data, out_fs, out_nbits, out_extra = WAV.wavread(io, subrange=sr)
         @assert length(out_data) == length(sr) * nchans
         @assert size(out_data, 1) == length(sr)
