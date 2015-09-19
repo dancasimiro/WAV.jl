c34eda4b25737d31e8441bd198fec64fb6033b65
Daniel C. Casimiro <dan.casimiro@gmail.com>
2015-09-19 14:46:07 -0400

    Replace another usage of Uint32 with UInt32

--- src/AudioDisplay.jl
+++ src/AudioDisplay.jl
@@ -1,7 +1,8 @@
 import Base.writemime
+using Compat
 
 type WAVArray{T,N}
-    Fs::Uint32
+    Fs::UInt32
     data::Array{T,N}
 end
 
@@ -10,7 +11,7 @@ wavwrite(x::WAVArray, io::IO) = wavwrite(x.data, io; Fs=x.Fs)
 function writemime(io::IO, ::MIME"text/html", x::WAVArray)
     buf = IOBuffer()
     wavwrite(x, buf)
-    data = base64(bytestring(buf)) 
+    data = base64(bytestring(buf))
     markup = """<audio controls="controls" {autoplay}>
                 <source src="data:audio/wav;base64,$data" type="audio/wav" />
                 Your browser does not support the audio element.
