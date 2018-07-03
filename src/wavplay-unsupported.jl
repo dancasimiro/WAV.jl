4e6ea493b512dbd0677b8e5b29c23ba7637fa4f3
Martin Holters <martin.holters@hsu-hh.de>
2018-07-03 15:03:31 +0200

    Use `Sys.KERNEL` instead of `OS_NAME`

--- src/wavplay-unsupported.jl
+++ src/wavplay-unsupported.jl
@@ -1,5 +1,5 @@
 # -*- mode: julia; -*-
 module WAVPlay
 import ..wavplay
-wavplay(data, fs) = warn("wavplay is not currently implemented on $OS_NAME")
+wavplay(data, fs) = warn("wavplay is not currently implemented on $(Sys.KERNEL)")
 end # module
