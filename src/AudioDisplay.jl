d8c4de27ef913fbc57e565fd79654f30922d1dc5
Ricardo Rosa <rmsrosa@gmail.com>
2021-07-04 00:27:57 -0300

    add caption to wavarray and more dispatches

--- src/AudioDisplay.jl
+++ src/AudioDisplay.jl
@@ -4,7 +4,24 @@ using Base64: base64encode
 struct WAVArray{T,N}
     Fs::Number
     data::AbstractArray{T,N}
+    caption::AbstractString
 end
+
+WAVArray(Fs::Number, data::AbstractArray) = WAVArray(Fs, data, "")
+
+function WAVArray(io::IO, caption::AbstractString; subrange=(:), format="double")
+    data, Fs = wavread(io, subrange=subrange, format=format)
+    return WAVArray(Fs, data, caption)
+end
+
+WAVArray(io::IO; subrange=(:), format="double") = WAVArray(io::IO, ""; subrange=subrange, format=format)
+
+function WAVArray(filename::AbstractString, caption::AbstractString; subrange=(:), format="double")
+    data, Fs = wavread(filename, subrange=subrange, format=format)
+    return WAVArray(Fs, data, caption)
+end
+
+WAVArray(filename::AbstractString; subrange=(:), format="double") = WAVArray(filename, ""; subrange=subrange, format=format)
 
 wavwrite(x::WAVArray, io::IO) = wavwrite(x.data, io; Fs=x.Fs)
 
@@ -12,9 +29,12 @@ function show(io::IO, ::MIME"text/html", x::WAVArray)
     buf = IOBuffer()
     wavwrite(x, buf)
     data = base64encode(String(take!(copy(buf))))
-    markup = """<audio controls="controls" {autoplay}>
-                <source src="data:audio/wav;base64,$data" type="audio/wav" />
-                Your browser does not support the audio element.
-                </audio>"""
+    markup = """<figure>
+    $(ifelse(x.caption !== "", "<figcaption>$(x.caption)</figcaption>", ""))
+    <audio controls="controls" {autoplay}>
+    <source src="data:audio/wav;base64,$data" type="audio/wav" />
+    Your browser does not support the audio element.
+    </audio>
+    </figure>"""
     print(io, markup)
 end
